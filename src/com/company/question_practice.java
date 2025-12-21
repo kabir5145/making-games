3fc563dc8154e9e7d469c5fbcf9facab289a1640
kabir5145 <kabir10112006@gmail.com>
2025-12-21 17:13:33 +0530

    printing singly linked list. Question of hackerRank

---
+++ src/com/company/question_practice.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class question_practice {
+    private ListNode head;
+
+    private static class ListNode {
+        public int data;
+        public ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    //Printing the singly linked list
+    public void printList(){
+        ListNode current = head;
+        while(current != null){
+            System.out.print(current.data+"-->");
+            current = current.next;
+        }
+        System.out.println("Null");
+    }
+    public static void main(String[] args) {
+        //Printing the singly linked list
+        question_practice q = new question_practice();
+        q.head = new ListNode(10);
+        q.head.next = new ListNode(20);
+        q.head.next.next = new ListNode(30);
+
+        q.printList();
+    }
+}

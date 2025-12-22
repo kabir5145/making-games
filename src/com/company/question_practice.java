5f01ca015b8aa86f3c7fd25f493c3584b5546ac6
kabir5145 <kabir10112006@gmail.com>
2025-12-22 19:05:56 +0530

    Insert a Node at the Tail of a Linked List HackerRank question

--- src/com/company/question_practice.java
+++ src/com/company/question_practice.java
@@ -21,6 +21,21 @@ public class question_practice {
         }
         System.out.println("Null");
     }
+
+    //Insert a Node at the Tail of a Linked List
+    static ListNode insertNodeAtTail(ListNode head, int data) {
+        ListNode newNode = new ListNode(data);
+        if(head == null){
+            head = newNode;
+            return head;
+        }
+        ListNode current = head;
+        while (null != current.next){
+            current = current.next;
+        }
+        current.next = newNode;
+        return head;
+    }
     public static void main(String[] args) {
         //Printing the singly linked list
         question_practice q = new question_practice();
@@ -28,6 +43,9 @@ public class question_practice {
         q.head.next = new ListNode(20);
         q.head.next.next = new ListNode(30);
 
+        q.printList();
+
+        q.insertNodeAtTail(q.head, 10);
         q.printList();
     }
 }
